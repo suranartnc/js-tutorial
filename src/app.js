958be0dbc256d380c254dc5574da1cb17e0e8e89
Suranart Niamcome <suranartnc@gmail.com>
2017-11-14 11:24:17 +0700

    Add some styles

--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
-require('es6-promise').polyfill()
-require('isomorphic-fetch')
-
 import React from 'react'
 
+function Container({ style, title, color }) {
+  const styleApplied = {
+    width: '100%',
+    height: 300,
+    backgroundColor: color,
+    clear: 'both',
+    ...style
+  }
+
+  return <div style={styleApplied}>{title}</div>
+}
+
 function App() {
-  return <div>Rendered by React using JSX</div>
+  const style = {
+    maxWidth: 960,
+    margin: '0 auto',
+    fontFamily: 'arial, sans-serif',
+    fontSize: '24px'
+  }
+
+  return (
+    <div style={style}>
+      <Container color="#888" title="Header" />
+      <div>
+        <Container
+          style={{ float: 'right', width: '70%', clear: 'none' }}
+          color={`#aaa`}
+          title="Main"
+        />
+        <Container
+          style={{ float: 'left', width: '30%', clear: 'none' }}
+          color={'#ccc'}
+          title="Sidebar"
+        />
+      </div>
+      <Container color={`#888`} title="Footer" />
+    </div>
+  )
 }
 
 export default App
