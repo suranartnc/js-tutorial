d3efec9aa6cceff8ff7fcbafe3fdd1bc9154d6d4
SuranartN <suranartnc@gmail.com>
2017-11-15 23:19:46 +0700

    Apply concept of container and presentational components

--- src/app.js
+++ src/app.js
@@ -1,50 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Container({ style, title, color }) {
-  const styleApplied = {
-    width: '100%',
-    height: 300,
-    backgroundColor: color,
-    clear: 'both',
-    ...style
-  }
-
-  return <div style={styleApplied}>{title}</div>
+function CategoryItem({ category }) {
+  return <li>{category.cat_name}</li>
 }
 
-Container.propTypes = {
-  title: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
-  style: PropTypes.object
+CategoryItem.propTypes = {
+  category: PropTypes.shape({
+    cat_name: PropTypes.string.isRequired
+  })
+}
+
+function CategoryList({ categories }) {
+  return (
+    <ul>
+      {categories.map(category => (
+        <CategoryItem key={category.cat_id} category={category} />
+      ))}
+    </ul>
+  )
+}
+
+CategoryList.propTypes = {
+  categories: PropTypes.arrayOf(CategoryItem.propTypes.category)
+}
+
+class CategoryContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.api = 'http://myaday.net/pop/api.php'
+    this.state = {
+      categories: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(this.api)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          categories: json.data
+        })
+      })
+  }
+
+  render() {
+    return <CategoryList categories={this.state.categories} />
+  }
 }
 
 function App() {
-  const style = {
-    maxWidth: 960,
-    margin: '0 auto',
-    fontFamily: 'arial, sans-serif',
-    fontSize: '24px'
-  }
-
-  return (
-    <div style={style}>
-      <Container color="#888" title="Header" />
-      <div>
-        <Container
-          style={{ float: 'right', width: '70%', clear: 'none' }}
-          color={`#aaa`}
-          title="Main"
-        />
-        <Container
-          style={{ float: 'left', width: '30%', clear: 'none' }}
-          color={'#ccc'}
-          title="Sidebar"
-        />
-      </div>
-      <Container color={`#888`} title="Footer" />
-    </div>
-  )
+  return <CategoryContainer />
 }
 
 export default App
