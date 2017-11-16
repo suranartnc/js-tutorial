b0a4038dc0a2eed885ed769258a77d6c6dea5289
SuranartN <suranartnc@gmail.com>
2017-11-16 23:01:35 +0700

    Add font awesome

--- src/containers/TimerContainer.js
+++ src/containers/TimerContainer.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Counter({ count = 0 }) {
   const style = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    backgroundColor: '#00bfff',
+    backgroundColor: '#000',
     textAlign: 'center',
-    border: '10px solid #fff',
+    border: '5px solid #bf2615',
     borderRadius: '50%',
     fontSize: '6rem',
     width: '220px',
@@ -15,55 +16,78 @@ function Counter({ count = 0 }) {
     margin: '0 auto',
     color: '#fff'
   }
-  return (
-    <div style={style}>
-      <p>{count}</p>
-    </div>
-  )
+  return <div style={style}>{count}</div>
 }
 
-function Button({ label = 'Submit' }) {
+Counter.propTypes = {
+  count: PropTypes.number
+}
+
+function Button({ icon }) {
   const style = {
-    borderRadius: '50%',
+    cursor: 'pointer',
+    border: '0',
     width: '64px',
     height: '64px',
     padding: '5px',
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     fontWeight: 'bold',
-    fontSize: '1.2rem',
-    color: '#00bfff'
+    fontSize: '2.4rem',
+    color: '#bf2615'
   }
-  return <button style={style}>{label}</button>
+  return (
+    <button style={style}>
+      <i className={`fa fa-${icon}`} />
+    </button>
+  )
 }
 
-function Panel() {
+Button.propTypes = {
+  icon: PropTypes.string
+}
+
+function Panel({ pause = false }) {
   const style = {
     display: 'flex',
     justifyContent: 'space-around'
   }
   return (
     <div style={style}>
-      <Button label="Start" />
-      <Button label="Lap" />
-      <Button label="Reset" />
+      {pause ? <Button icon="pause" /> : <Button icon="play" />}
+      <Button icon="stop" />
+      <Button icon="clock-o" />
     </div>
   )
+}
+
+Panel.propTypes = {
+  pause: PropTypes.bool
 }
 
 export default class TimerContainer extends React.Component {
   render() {
     const style = {
-      display: 'flex',
-      height: '100%',
-      padding: '50px',
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      backgroundColor: '#00bfff'
+      app: {
+        backgroundColor: '#000',
+        width: '100%',
+        height: '100%'
+      },
+      wrapper: {
+        display: 'flex',
+        maxWidth: '320px',
+        margin: '0 auto',
+        height: '100%',
+        padding: '50px',
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+      }
     }
     return (
-      <div style={style}>
-        <Counter />
-        <Panel />
+      <div style={style.app}>
+        <div style={style.wrapper}>
+          <Counter />
+          <Panel />
+        </div>
       </div>
     )
   }
--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ app.get('*', function(req, res) {
       <head>
         <title>JavaScript Tutorial</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
         <style>
           * {
             box-sizing: border-box;
