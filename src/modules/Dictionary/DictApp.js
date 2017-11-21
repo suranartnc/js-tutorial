b45e9c273963354be591d4223effcad127cb5016
Suranart Niamcome <suranartnc@gmail.com>
2017-11-21 10:36:42 +0700

    Add input handler

--- src/modules/Dictionary/DictApp.js
+++ src/modules/Dictionary/DictApp.js
@@ -1,10 +1,55 @@
 import React from 'react'
 import styles from './DictStyles'
 
-export default function DictApp() {
+function ResultList({ results }) {
+  return <ul>{results.map(item => <li>{item.result}</li>)}</ul>
+}
+
+function DictApp({ keywords, results, inputHandler }) {
   return (
     <div style={styles.app}>
-      <div style={styles.wrapper}>Dictionary App</div>
+      <div style={styles.wrapper}>
+        <input
+          placeholder="keywords..."
+          value={keywords}
+          onChange={inputHandler}
+        />
+        <button>Search</button>
+        <ResultList results={results} />
+      </div>
     </div>
   )
+}
+
+export default class DictContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      keywords: '',
+      results: [
+        {
+          result: 'กิน'
+        }
+      ]
+    }
+
+    this.inputHandler = this.inputHandler.bind(this)
+  }
+
+  inputHandler(event) {
+    this.setState({
+      keywords: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <DictApp
+        keywords={this.state.keywords}
+        results={this.state.results}
+        inputHandler={this.inputHandler}
+      />
+    )
+  }
 }
