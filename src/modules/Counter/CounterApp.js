9bd46eb2a3d562d88e775d68f1f7d878d569cc8f
Suranart Niamcome <suranartnc@gmail.com>
2017-11-24 18:26:26 +0700

    Make redux works

--- src/modules/Counter/CounterApp.js
+++ src/modules/Counter/CounterApp.js
@@ -1,10 +1,54 @@
 import React from 'react'
 import AppWrapper from '../../components/AppWrapper'
 
+import { createStore, combineReducers } from 'redux'
+import { Provider, connect } from 'react-redux'
+
+const counterReducer = function(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREASE':
+      return {
+        count: state.count - 1
+      }
+    default:
+      return state
+  }
+}
+
+const reducer = combineReducers({
+  counter: counterReducer
+})
+
+const store = createStore(reducer)
+
+function Counter({ count, dispatch }) {
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => dispatch({ type: 'INCREASE' })}>+</button>
+      <button onClick={() => dispatch({ type: 'DECREASE' })}>-</button>
+    </div>
+  )
+}
+
+function selector(state) {
+  return {
+    count: state.counter.count
+  }
+}
+
+const CounterWithState = connect(selector)(Counter)
+
 export default function CounterApp() {
   return (
-    <AppWrapper>
-      <p>Counter App</p>
-    </AppWrapper>
+    <Provider store={store}>
+      <AppWrapper>
+        <CounterWithState />
+      </AppWrapper>
+    </Provider>
   )
 }
