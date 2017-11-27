cdebbd68914ff598ce4d19bca8294edf6c4df68c
Suranart Niamcome <suranartnc@gmail.com>
2017-11-27 20:31:26 +0700

    Remove redux code

--- src/modules/Counter/CounterApp.js
+++ src/modules/Counter/CounterApp.js
@@ -2,19 +2,10 @@ import React from 'react'
 import { createStore, combineReducers as combineStateUpdater } from 'redux'
 import { Provider, connect } from 'react-redux'
 
-// View //
 class Counter extends React.Component {
-  increaseCount = () => {
-    this.props.dispatch({
-      type: 'INCREASE'
-    })
-  }
+  increaseCount = () => {}
 
-  decreaseCount = () => {
-    this.props.dispatch({
-      type: 'DECREASE'
-    })
-  }
+  decreaseCount = () => {}
 
   render() {
     return (
@@ -27,42 +18,6 @@ class Counter extends React.Component {
   }
 }
 
-// Store & State Updater //
-function countUpdater(state = 0, action) {
-  switch (action.type) {
-    case 'INCREASE':
-      return state + 1
-
-    case 'DECREASE':
-      return state - 1
-
-    default:
-      return state
-  }
-}
-
-const rootStateUpdater = combineStateUpdater({
-  count: countUpdater
-
-  // Add more state here...
-  // moreStateKey: moreStateUpdater
-})
-
-const store = createStore(rootStateUpdater)
-
-// Connector & stateSelector //
-function stateSelector(state) {
-  return {
-    count: state.count
-  }
-}
-
-const ConnectedCounter = connect(stateSelector)(Counter)
-
 export default function CounterApp() {
-  return (
-    <Provider store={store}>
-      <ConnectedCounter />
-    </Provider>
-  )
+  return <Counter count={0} />
 }
