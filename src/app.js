1d0d0bc4071c3956b75baaf3a3f86eb71da45cd1
SuranartN <suranartnc@gmail.com>
2017-12-02 16:11:18 +0700

    Remove unused code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react'
+
 import ToDoApp from './modules/ToDo/ToDoApp'
+// import DictApp from './modules/Dictionary/DictApp'
+// import TimerApp from './modules/Timer/TimerApp'
 
 function App() {
   return <ToDoApp />
--- src/modules/Counter/CounterActions.js
+++
@@ -1,6 +0,0 @@
-export function updateCount(type = 'INCREASE', number = 1) {
-  return {
-    type,
-    number
-  }
-}
