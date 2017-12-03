886599cb906f7eb8f957e7d4ac9962220e585fff
SuranartN <suranartnc@gmail.com>
2017-12-03 16:07:43 +0700

    Add relate entries section

--- src/modules/Blog/pages/EntryPage.js
+++ src/modules/Blog/pages/EntryPage.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Layout from '../components/Layout'
 
-function EntryPage({ entry }) {
+function EntryPage({ entry, relateEntries }) {
   return (
     <Layout>
       <h1>{entry.title}</h1>
       <article>{entry.body}</article>
+
+      {relateEntries.map(function(entry) {
+        return (
+          <article key={entry.id}>
+            <h2>
+              <Link to={`/entry/${entry.id}/`}>{entry.title}</Link>
+            </h2>
+            <p>{entry.excerpt}</p>
+          </article>
+        )
+      })}
     </Layout>
   )
 }
@@ -15,11 +27,13 @@ class EntryPageContainer extends React.Component {
   api = 'http://localhost:3000/posts'
 
   state = {
-    entry: {}
+    entry: {},
+    relateEntries: []
   }
 
   componentDidMount() {
     this.fetchEntry()
+    this.fetchRelateEntries()
   }
 
   fetchEntry = () => {
@@ -34,8 +48,23 @@ class EntryPageContainer extends React.Component {
       })
   }
 
+  fetchRelateEntries = () => {
+    fetch(this.api)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          relateEntries: json
+        })
+      })
+  }
+
   render() {
-    return <EntryPage entry={this.state.entry} />
+    return (
+      <EntryPage
+        entry={this.state.entry}
+        relateEntries={this.state.relateEntries}
+      />
+    )
   }
 }
 
