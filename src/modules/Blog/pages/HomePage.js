8e66b7b3678bfa77bfe551d939cc25b7aa48b76e
SuranartN <suranartnc@gmail.com>
2017-12-03 15:48:20 +0700

    Fetch data from json api

--- src/modules/Blog/pages/HomePage.js
+++ src/modules/Blog/pages/HomePage.js
@@ -3,13 +3,47 @@ import { Link } from 'react-router-dom'
 
 import Layout from '../components/Layout'
 
-function HomePage() {
+function HomePage({ entries }) {
   return (
     <Layout>
-      <p>This is home page.</p>
-      <Link to="/entry">Entry page</Link>
+      {entries.map(function(entry) {
+        return (
+          <article key={entry.id}>
+            <h2>
+              <Link to="/entry">{entry.title}</Link>
+            </h2>
+            <p>{entry.excerpt}</p>
+          </article>
+        )
+      })}
     </Layout>
   )
 }
 
-export default HomePage
+class HomePageContainer extends React.Component {
+  api = 'http://localhost:3000/posts'
+
+  state = {
+    entries: []
+  }
+
+  componentDidMount() {
+    this.fetchEntries()
+  }
+
+  fetchEntries = () => {
+    fetch(this.api)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          entries: json
+        })
+      })
+  }
+
+  render() {
+    return <HomePage entries={this.state.entries} />
+  }
+}
+
+export default HomePageContainer
